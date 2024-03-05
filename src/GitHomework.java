4cb2bceb8359b40132c60c6a08712622bd2403fd
OleksandrMaimeskul <maymeskulsasha@gmail.com>
2024-03-05 18:27:19 +0200

    Was error before, now fixed

--- src/GitHomework.java
+++ src/GitHomework.java
@@ -1,7 +1,4 @@
 package src;
-
-import java.sql.SQLOutput;
-
 public class GitHomework {
     public static void main(String[] args) {
         System.out.println("My name is Oleksandr Maimeskul and this is my Git Homework :)");
