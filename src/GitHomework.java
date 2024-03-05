cd8a58d1cae1ec010b9723f3342c0877716a557f
OleksandrMaimeskul <maymeskulsasha@gmail.com>
2024-03-05 18:14:58 +0200

    Homework

---
+++ src/GitHomework.java
@@ -0,0 +1,8 @@
+package src;
+
+public class GitHomework {
+    public static void main(String[] args) {
+        System.out.println("My name is Oleksandr Maimeskul and this is my Git Homework :)");
+
+    }
+}
